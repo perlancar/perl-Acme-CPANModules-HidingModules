060642196cb6f8f60bd04489a96da22d872d99a8
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-01-10 17:25:29 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/HidingModules.pm
@@ -0,0 +1,27 @@
+package Acme::CPANModules::HidingModules;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => 'Simulate the unavailability of modules',
+    description => <<'_',
+
+The tools listed here can simulate the absence of modules, usually for testing
+purposes. For example, you have Foo::Bar installed but want to test how your
+code would behave when Foo::Bar is not installed.
+
+These tools usually work by installing a require() hook in `@INC`. If the hook
+sees that you are trying to load one of the target modules, it dies instead.
+
+_
+    entries => [
+        {module=>'lib::filter'},
+        {module=>'lib::disallow'},
+        {module=>'Devel::Hide'},
+        {module=>'Test::Without::Module'},
+    ],
+};
+
+1;
+# ABSTRACT:
